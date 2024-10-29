750fd76d2ff818984c6a7f86cbb3b002d03db343
ja-wrobel <145217852+ja-wrobel@users.noreply.github.com>
2024-10-29 16:55:56 +0100

    Setup vue+vite + example first page (#8)
    * Setup vue+vite + example first page
    
    * Build vite on workflow

---
+++ resources/js/components/Home.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="main">
+        <div class="text-container encourage">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque ac felis vitae enim porta luctus. Curabitur tristique dapibus quam, in malesuada urna cursus quis. In quis dolor nulla. Cras in urna consectetur, pretium purus vel, molestie risus. Vivamus sed accumsan sapien. Nam a risus volutpat purus eleifend ullamcorper a non felis. Morbi gravida, libero dictum mattis semper, lacus nisi tristique lorem, eget venenatis nibh ex sit amet urna. Curabitur sed libero eu velit iaculis volutpat eget at risus. Nam ullamcorper pharetra massa, scelerisque bibendum metus. Aliquam erat volutpat. Aliquam rhoncus malesuada erat, ut facilisis magna tempus nec. In in mattis magna. Nulla facilisi. Nam eget diam mollis, pellentesque lectus vel, ultricies mi.
+        </div>
+        <div class="flex main-flexbox">
+            <div class="button">
+                Start for free
+            </div>
+            <div class="button">
+                Try as guest
+            </div>
+        </div>
+    </div>
+</template>
