63bba70eb9b6efa8f1a494e96af855cc38c86c68
ja-wrobel <jakub.wrobel2563@gmail.com>
2024-11-28 14:27:45 +0100

    Pinia installation and setup + PHP Unit setup correction

--- resources/js/Components/GameTile.vue
+++ resources/js/Components/GameTile.vue
@@ -51,9 +51,9 @@
 <template>
     <div class="tile" v-for="game in games.data" :key="game.id">
         <div class="header flex">
-            <div class="text">{{ game.difficulty }}</div>
-            <div class="text title">{{ game.name }}</div>
-            <div class="text">{{ game.type }}</div>
+            <div class="text txt-align-l">{{ game.difficulty }}</div>
+            <div class="text title txt-align-c">{{ game.name }}</div>
+            <div class="text txt-align-r">{{ game.type }}</div>
         </div>
         <div class="body">
             {{ game.description }}
